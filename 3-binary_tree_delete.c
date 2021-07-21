d1a7a1b5c6bc0cc76599e2b6c7dddc0fd73df4f8
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-07-21 12:08:50 -0700

    Update 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,7 +9,7 @@ void binary_tree_delete(binary_tree_t *tree)
 if (tree == NULL)
 return;
 /* first delete both subtrees */
-binary_tree_delete(tree->left)
+binary_tree_delete(tree->left);
 binary_tree_delete(tree-right);
 /* then delete the node */
 free(tree);   
