bb4a2d9a2d891ad015300a74cd3aa18d4a61717d
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-07-21 13:01:33 -0700

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,18 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_is_leaf - check node leaf or not
+ * @node: node to check
+ * Return: 1 if node if leaf 0 if not
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+if (node == NULL)
+return (0);
+else if (node->left == NULL && node->right == NULL)
+return (1);
+return (0);
+}
+/**
  * binary_tree_height -measures the height of a binary tree
  * @tree:  pointer to the root node of the tree to measure height.
  * Return: return binary tree
