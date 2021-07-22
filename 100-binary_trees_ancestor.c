dbad199187e3626e8fcd61983759d718f8797c64
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-07-22 08:37:06 -0700

    Update 100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -5,7 +5,8 @@
  * @second: a pointer to the second node
  * Return: lowest node
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+                                     const binary_tree_t *second)
 {
 binary_tree_t *node;
 if (first == NULL || second == NULL)
